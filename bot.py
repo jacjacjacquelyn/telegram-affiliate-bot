52d55f15f537c02c64e0479ea8ae2a0c217420c0
Jacquelyn Pang <jacq@Jacquelyns-MacBook-Pro.local>
2026-05-28 16:47:17 +0800

    fix polling conflict

--- bot.py
+++ bot.py
@@ -145,17 +145,14 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     print("Bot starting...")
 
-    print("TOKEN:", bool(TELEGRAM_TOKEN))
-    print("APP_ID:", bool(APP_ID))
-    print("APP_SECRET:", bool(APP_SECRET))
-
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle))
 
     print("Bot running...")
-    app.run_polling()
+
+    app.run_polling(drop_pending_updates=True)
 
 
 if __name__ == "__main__":
